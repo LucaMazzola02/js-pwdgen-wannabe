aeaced12099055a60f6b70c456403e495a22c31c
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-05-11 16:45:45 +0200

    complete entire exercise

---
+++ script.js
@@ -0,0 +1,13 @@
+const UserName = prompt('Insert your name');
+
+console.log(UserName);
+
+const SurName = prompt('Insert your surname');
+
+console.log(SurName);
+
+const FavouriteColor = prompt('Insert your favourite color');
+
+console.log(FavouriteColor);
+
+document.getElementById('answer').innerHTML= UserName + SurName + FavouriteColor + 23
