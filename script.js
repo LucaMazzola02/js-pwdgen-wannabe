101429e66270a70f6f9c3475ce2fbb22ec618c54
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-05-11 16:47:30 +0200

    omitted the console.log

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 const UserName = prompt('Insert your name');
 
-console.log(UserName);
-
 const SurName = prompt('Insert your surname');
-
-console.log(SurName);
 
 const FavouriteColor = prompt('Insert your favourite color');
 
-console.log(FavouriteColor);
-
-document.getElementById('answer').innerHTML= UserName + SurName + FavouriteColor + 23
+document.getElementById('answer').innerHTML= UserName + SurName + FavouriteColor + 23 ;
